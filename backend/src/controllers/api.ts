5707b4d450fd05f607314843058b790066e52f6f
LitalAfriat <afriatlital@gmail.com>
2026-04-16 11:26:36 +0300

    context

--- backend/src/controllers/api.ts
+++ backend/src/controllers/api.ts
@@ -1,8 +1,28 @@
 import { Request, Response } from "express";
-
-async function sendEmailCode(req: Request, res: Response) {
-    console.log(req.body)
-    return res.status(200).json({ message: "Hello LIT!" });
-}
+import nodemailer from 'nodemailer';
 
 export { sendEmailCode };
+
+const transporter = nodemailer.createTransport({
+  service: process.env.EMAIL_SERVICE,
+  auth: {
+    user: process.env.EMAIL_USER, 
+    pass: process.env.EMAIL_PASS, 
+  },
+});
+
+function random6DigitInteger(): number {
+    return Math.floor(Math.random() * 900000) + 100000;
+}
+
+async function sendEmailCode(req: Request, res: Response) {
+    const emailRespond = await transporter.sendMail({
+    from: 'afriatnails@gmail.com', // use env
+    to: req.body.email ,
+    subject: 'Nails App Email Verification Code.',
+    html: `<p> ${random6DigitInteger()} </p>`,
+    
+    });
+
+    return res.status(200).json({ message: "Hello LIT!" }); // send info about the success of the email sending, and not just a message
+} 
---
+++ frontend/src/context/EmailContext.tsx
@@ -0,0 +1,27 @@
+// context/EmailContext.tsx
+import React, { createContext, useContext, useState, ReactNode } from 'react';
+
+interface EmailContextType {
+  email: string;
+  setEmail: (email: string) => void;
+}
+
+const EmailContext = createContext<EmailContextType | undefined>(undefined);
+
+export const EmailProvider = ({ children }: { children: ReactNode }) => {
+  const [email, setEmail] = useState<string>('');
+
+  return (
+    <EmailContext.Provider value={{ email, setEmail }}>
+      {children}
+    </EmailContext.Provider>
+  );
+};
+
+export const useEmail = (): EmailContextType => {
+  const context = useContext(EmailContext);
+  if (!context) {
+    throw new Error('useEmail must be used within an EmailProvider');
+  }
+  return context;
+};
